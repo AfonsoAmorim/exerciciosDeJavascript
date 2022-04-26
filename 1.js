8ad93a8d43a4a1a059e726fef1f4f9d3a74667e9
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-26 19:39:00 -0300

    exercício 3

--- 1.js
+++ 1.js
@@ -1,11 +1,11 @@
-// Teste 1
+// Teste 1 - console.log
 const primeiroWorld = "Hello World!";
 
 console.log("Meu primeiro programa,",primeiroWorld);
 
 console.log(`Meu primeiro programa com UPPERCASE,${primeiroWorld.toLocaleUpperCase()}`);
 
-//Teste 2
+//Teste 2 - Operações
 
 let num = 10
 let num2 = 50
@@ -17,3 +17,23 @@ console.log(`Resultado divisão -> ${num/num2}`);
 
 console.log("Os números são iguais?",num==num2);
 
+
+//Teste 3 - Listas
+
+const lista = ["Alonso", "Viviane", 100, 200, 300, "Aline"];
+console.log("Lista inicial: " + lista);
+
+lista.push('Jonas');
+console.log("Lista com push",lista);
+
+lista.unshift("Joana");
+console.log("Inserindo dado na primeira posição", lista);
+
+lista.pop()
+console.log("Retirando último elemento da lista (Jonas)", lista);
+
+let ordenarNames = lista.sort();
+console.log("Lista ordenada --> ",lista);
+
+let verificaArray = Array.isArray(lista);
+console.log("Verifica se varoável é array --> ",verificaArray);
