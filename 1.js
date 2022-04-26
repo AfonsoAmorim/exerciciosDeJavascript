5830e29d070774053da744d3836ba5684d4c6e70
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-26 19:08:50 -0300

    exercícios de Javascript

---
+++ 1.js
@@ -0,0 +1,7 @@
+// Teste 1
+const primeiroWorld = "Hello World!";
+
+console.log("Meu primeiro programa,",primeiroWorld);
+
+console.log(`Meu primeiro programa com UPPERCASE,${primeiroWorld.toLocaleUpperCase()}`);
+
