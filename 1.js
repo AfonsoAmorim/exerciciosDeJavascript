7a69f7a2d97636039a9ac7d60434b36d858c0ebb
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-26 19:19:50 -0300

    exercício 2

--- 1.js
+++ 1.js
@@ -5,3 +5,15 @@ console.log("Meu primeiro programa,",primeiroWorld);
 
 console.log(`Meu primeiro programa com UPPERCASE,${primeiroWorld.toLocaleUpperCase()}`);
 
+//Teste 2
+
+let num = 10
+let num2 = 50
+
+console.log(`Resultado multiplicação -> ${num*num2}`);
+console.log(`Resultado adição -> ${num+num2}`);
+console.log(`Resultado subtração -> ${num-num2}`);
+console.log(`Resultado divisão -> ${num/num2}`);
+
+console.log("Os números são iguais?",num==num2);
+
