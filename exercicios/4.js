a5109703ef7546ea0f8b8ac9336491645c1d4476
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-07 13:21:08 -0300

    exercicio de funcao

--- exercicios/4.js
+++ exercicios/4.js
@@ -1,11 +1,20 @@
 // teste FOREACH
 const perfumes = ["h25", "billboard100","aporte10"];
 
-perfumes.forEach((perfumes)=>{
+/* perfumes.forEach((perfumes)=>{
     console.log(perfumes);
 });
-
+ */
 
 perfumes.forEach((perfumes,index,array)=>{
     console.log(index,perfumes,array);
 });
+
+// teste função 
+
+function conta(var1,var2){
+    return var1 + var2;
+}
+
+let conta1 = conta(1,2);
+console.log(conta1);
